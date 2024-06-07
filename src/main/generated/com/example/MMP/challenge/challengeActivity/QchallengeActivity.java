125ac71991f496b182b7892110e73902fb8aca23
ãDowonNam <dwnam0114@gmail.com>
2024-06-07 16:12:15 +0900

    add the attendance

--- src/main/generated/com/example/MMP/challenge/challengeActivity/QchallengeActivity.java
+++ src/main/generated/com/example/MMP/challenge/challengeActivity/QchallengeActivity.java
@@ -11,20 +11,20 @@ import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
- * QchallengeActivity is a Querydsl query type for challengeActivity
+ * QChallengeActivity is a Querydsl query type for ChallengeActivity
  */
 @Generated("com.querydsl.codegen.DefaultEntitySerializer")
-public class QchallengeActivity extends EntityPathBase<challengeActivity> {
+public class QChallengeActivity extends EntityPathBase<ChallengeActivity> {
 
-    private static final long serialVersionUID = -800045636L;
+    private static final long serialVersionUID = -1156282980L;
 
     private static final PathInits INITS = PathInits.DIRECT2;
 
-    public static final QchallengeActivity challengeActivity = new QchallengeActivity("challengeActivity");
+    public static final QChallengeActivity challengeActivity = new QChallengeActivity("challengeActivity");
 
     public final DateTimePath<java.time.LocalDateTime> activeDate = createDateTime("activeDate", java.time.LocalDateTime.class);
 
-    public final NumberPath<Integer> attendance = createNumber("attendance", Integer.class);
+    public final com.example.MMP.attendance.QAttendance attendance;
 
     public final com.example.MMP.challenge.challenge.QChallenge challenge;
 
@@ -36,24 +36,25 @@ public class QchallengeActivity extends EntityPathBase<challengeActivity> {
 
     public final NumberPath<Integer> weight = createNumber("weight", Integer.class);
 
-    public QchallengeActivity(String variable) {
-        this(challengeActivity.class, forVariable(variable), INITS);
+    public QChallengeActivity(String variable) {
+        this(ChallengeActivity.class, forVariable(variable), INITS);
     }
 
-    public QchallengeActivity(Path<? extends challengeActivity> path) {
+    public QChallengeActivity(Path<? extends ChallengeActivity> path) {
         this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
-    public QchallengeActivity(PathMetadata metadata) {
+    public QChallengeActivity(PathMetadata metadata) {
         this(metadata, PathInits.getFor(metadata, INITS));
     }
 
-    public QchallengeActivity(PathMetadata metadata, PathInits inits) {
-        this(challengeActivity.class, metadata, inits);
+    public QChallengeActivity(PathMetadata metadata, PathInits inits) {
+        this(ChallengeActivity.class, metadata, inits);
     }
 
-    public QchallengeActivity(Class<? extends challengeActivity> type, PathMetadata metadata, PathInits inits) {
+    public QChallengeActivity(Class<? extends ChallengeActivity> type, PathMetadata metadata, PathInits inits) {
         super(type, metadata, inits);
+        this.attendance = inits.isInitialized("attendance") ? new com.example.MMP.attendance.QAttendance(forProperty("attendance"), inits.get("attendance")) : null;
         this.challenge = inits.isInitialized("challenge") ? new com.example.MMP.challenge.challenge.QChallenge(forProperty("challenge")) : null;
     }
 
