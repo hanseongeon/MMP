83740a014e09fb0cc3d36d5d360e84163d3bb363
ãDowonNam <dwnam0114@gmail.com>
2024-06-17 11:49:47 +0900

    add the group

--- src/main/java/com/example/MMP/challengeGroup/GroupTag/GroupTagController.java
+++ src/main/java/com/example/MMP/challengeGroup/GroupTag/GroupTagController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/group/{groupId}/tags")
+@RequestMapping("/groupChallenge/{groupId}/tags")
 public class GroupTagController {
     private final GroupTagService groupTagService;
 
@@ -17,7 +17,7 @@ public class GroupTagController {
     public String create(@PathVariable("groupId") Long groupId, String name) {
         GroupTag groupTag = groupTagService.create(groupId, name);
 
-        return "redirect:/group/detail/" + groupId;
+        return "redirect:/groupChallenge/detail/" + groupId;
     }
 
     @PostMapping("{groupTagId}/delete")
@@ -25,6 +25,6 @@ public class GroupTagController {
         GroupTag groupTag = groupTagService.getGroupTag(groupTagId);
         groupTagService.delete(groupTagId);
 
-        return "redirect:/group/detail/" + groupId;
+        return "redirect:/groupChallenge/detail/" + groupId;
     }
 }
