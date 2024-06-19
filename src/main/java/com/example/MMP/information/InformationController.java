dc535022821e09a5e335272170751ccfbfc171d2
Leeji0 <dldmsgktjf0716@gmail.com>
2024-06-19 13:03:34 +0900

    information

--- src/main/java/com/example/MMP/information/InformationController.java
+++ src/main/java/com/example/MMP/information/InformationController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,20 +22,30 @@ public class InformationController {
     private final FileUploadUtil fileUploadUtil;
 
     @GetMapping("/update")
-    public String create(InformationForm informationForm){
+    public String update(InformationForm informationForm) {
+        Information information = informationService.getInformation();
+        if (information != null){
+            informationForm.setImagePath(information.getImagePath());
+            informationForm.setHealthName(information.getHealthName());
+            informationForm.setCompanyNumber(information.getCompanyNumber());
+            informationForm.setAddress(information.getAddress());
+            informationForm.setCallNumber(information.getCallNumber());
+            informationForm.setEmail(information.getEmail());
+            informationForm.setText(information.getText());
+        }
         return "information_form";
     }
 
     @PostMapping("/update")
-    public String create(@Valid InformationForm informationForm, BindingResult bindingResult,  @RequestParam("image") MultipartFile image){
+    public String update(@Valid InformationForm informationForm, BindingResult bindingResult,  @RequestParam("image") MultipartFile image){
         if (bindingResult.hasErrors()){
             return "information_form";
         }
+        Information information = informationService.getInformation();
 
         // 이미지 파일의 경로가 비어있지 않으면 업로드를 시도합니다.
         if (image != null && !image.isEmpty()) {
             String fileName = StringUtils.cleanPath(image.getOriginalFilename());
-
             try {
                 informationForm.setImagePath(fileName);
                 this.fileUploadUtil.saveFile(fileName, image);
@@ -45,9 +56,13 @@ public class InformationController {
 
                 return "information_form"; // 업로드 실패 시 처리하는 방법에 따라 변경
             }
-            informationService.create(informationForm.getImagePath(), informationForm.getHealthName(), informationForm.getCompanyNumber(),
-                    informationForm.getAddress(), informationForm.getCallNumber(), informationForm.getEmail(), informationForm.getText());
+        }else {
+            String fileName = information.getImagePath();
+            informationForm.setImagePath(fileName);
         }
+
+        informationService.update(informationForm.getImagePath(), informationForm.getHealthName(), informationForm.getCompanyNumber(),
+                informationForm.getAddress(), informationForm.getCallNumber(), informationForm.getEmail(), informationForm.getText());
 
         return "redirect:/";
     }
--- src/main/java/com/example/MMP/information/InformationService.java
+++ src/main/java/com/example/MMP/information/InformationService.java
@@ -3,13 +3,20 @@ package com.example.MMP.information;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class InformationService {
     private final InformationRepository informationRepository;
 
-    public void create(String imagePath, String healthName, String companyNumber, String address, String callNumber, String email, String text) {
-        Information information = new Information();
+    public void update(String imagePath, String healthName, String companyNumber, String address, String callNumber, String email, String text) {
+        Information information = getInformation();
+        if (information == null) {
+            information = new Information();
+        }
+
         information.setImagePath(imagePath);
         information.setHealthName(healthName);
         information.setCompanyNumber(companyNumber);
@@ -20,4 +27,22 @@ public class InformationService {
 
         informationRepository.save(information);
     }
+
+    public Information getInformation() {
+        List<Information> informationList = getInformationList();
+
+        if (informationList.isEmpty()) {
+            Information information = new Information();
+            informationRepository.save(information);
+            informationList = getInformationList();
+        }
+        Information information = informationList.get(0);
+        return information;
+    }
+
+    public List<Information> getInformationList() {
+        List<Information> informationList = informationRepository.findAll();
+        return informationList;
+    }
+
 }
